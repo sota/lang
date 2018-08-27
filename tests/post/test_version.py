04bcdce704ba534c0ce9142935646b26781411d2
Scott Idler <scott.a.idler@gmail.com>
2018-08-27 08:03:12 -0700

    fixed up describe formatting

--- tests/post/test_version.py
+++ tests/post/test_version.py
@@ -13,4 +13,6 @@ from sota.utils.fmt import fmt, pfmt, dbg
 def test_version():
     version = call(fmt('{BINDIR}/sota --version'))[1]
     describe = call('git describe')[1]
+    parts = describe.split('-')
+    describe = parts[0].replace('v', '') + '.dev{0}+{1}'.format(*parts[1:]) if parts[1:] else ''
     assert version == describe
